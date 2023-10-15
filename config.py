021ffc9ee82196eccf038f9bafc26d0249f86fd3
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-10-14 22:59:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config():
         Otherwise create the config variable if not done already
     '''
 
+    
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Any text as a secret key'
