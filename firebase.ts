792d6299c76439d86f54bf203e06f85c5d323efb
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-10-26 18:28:43 -0400

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from './firebase.config';
-import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import { getStorage } from "firebase/storage";
 
 export const Firebase = initializeApp(firebaseConfig);
 export const auth = getAuth();
