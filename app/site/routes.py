58b29cdf0e47e4f01028d8e94e0eccc957fd5909
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-03 02:03:40 -0400

    Update routes.py

--- app/site/routes.py
+++ app/site/routes.py
@@ -1,4 +1,12 @@
 from flask import Blueprint, render_template
+import firebase_admin
+from firebase_admin import credentials, storage
+
+# Initialize Firebase
+cred = credentials.Certificate("path/to/your/firebase/credentials.json") # This is a JSON key you get from Firebase Console
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'frankie-s-sports-images.appspot.com'
+})
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
@@ -8,7 +16,14 @@ def home():
 
 @site.route('/images')
 def images():
-    return render_template('images.html')
+    # Assuming you have an image named "sample_image.jpg" in your Firebase Storage.
+    image_name = "sample_image.jpg"
+    bucket = storage.bucket()
+    blob = bucket.blob(image_name)
+    # Get the URL of the image
+    image_url = blob.generate_signed_url(timedelta(seconds=300), method='GET') # URL will be valid for 5 minutes
+   
+    return render_template('images.html', image_url=image_url)
 
 @site.route('/profile')
 def profile():
