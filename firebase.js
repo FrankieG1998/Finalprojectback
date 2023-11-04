c305524b7c9bf937eddeb24eac690358fc26b674
Shivam Lakhanpal <106916815+Lakhanpal-Shivam@users.noreply.github.com>
2023-11-04 01:45:26 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,5 +8,19 @@ const firebaseConfig = {
   messagingSenderId: "565582224329",
   appId: "1:565582224329:web:b53eaecc880b505117a618"
 };
+
+window.getImageFromFirebase = function(imageName, elementId) {
+  const storageRef = storage.ref(imageName);
+  storageRef.getDownloadURL()
+    .then((url) => {
+      // Get the image element by id
+      const img = document.getElementById(elementId);
+      img.src = url;
+    })
+    .catch((error) => {
+      console.error("Error loading image: ", error);
+    });
+};
+
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
