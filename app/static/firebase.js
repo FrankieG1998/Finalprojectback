4e45298e2810899a633a77946d47da9125a7e779
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-06 02:37:04 -0400

    Update firebase.js

--- app/static/firebase.js
+++ app/static/firebase.js
@@ -1,1 +1,25 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { getStorage, ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyCWDDT0J11CZXCglIVTYXRjpC9--X62A94",
+  authDomain: "frankie-s-sports-images.firebaseapp.com",
+  projectId: "frankie-s-sports-images",
+  storageBucket: "frankie-s-sports-images.appspot.com",
+  messagingSenderId: "565582224329",
+  appId: "1:565582224329:web:b53eaecc880b505117a618"
+};
 
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+window.getImageFromFirebase = function(imageName, elementId) {
+  const storageRef = ref(storage, imageName);
+  getDownloadURL(storageRef)
+    .then((url) => {
+      const img = document.getElementById(elementId);
+      img.src = url;
+    })
+    .catch((error) => {
+      console.error("Error loading image: ", error);
+    });
+};
