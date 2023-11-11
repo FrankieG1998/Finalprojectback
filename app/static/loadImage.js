b28210e6ddf177ca9f8ff52f4091cb97334c475d
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-11 01:37:40 -0400

    Update loadImage.js

--- app/static/loadImage.js
+++ app/static/loadImage.js
@@ -1,5 +1,12 @@
-// This code will run when the window is loaded
-window.addEventListener('load', function() {
-  getImageFromFirebase('Michael-Jordan.jpg', 'firebase-image');
-});
+// Function to check if the current URL is /images or /profile
+function shouldLoadImage() {
+  const path = window.location.pathname;
+  return path === '/images' || path === '/profile';
+}
 
+// Modified window.load event listener
+window.addEventListener('load', function() {
+  if (shouldLoadImage()) {
+    getImageFromFirebase('Michael-Jordan.jpg', 'firebase-image');
+  }
+});
