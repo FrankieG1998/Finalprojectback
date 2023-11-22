31be58b70b230e97853a9a461593c2a95714b7b2
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-22 07:32:51 -0400

    Update firebase.js

--- app/static/firebase.js
+++ app/static/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getStorage, ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCWDDT0J11CZXCglIVTYXRjpC9--X62A94",
   authDomain: "frankie-s-sports-images.firebaseapp.com",
@@ -22,4 +25,24 @@ window.getImageFromFirebase = function(imageName, elementId) {
     .catch((error) => {
       console.error("Error loading image: ", error);
     });
+};
+
+const auth = getAuth(app);
+
+window.uploadImageToFirebase = function(userId, file) {
+  // Create a storage reference with the user's ID and the file name
+  const storageRef = ref(storage, `images/${userId}/${file.name}`);
+  
+  // Upload the file to Firebase Storage
+  uploadBytes(storageRef, file).then((snapshot) => {
+    console.log('Uploaded a blob or file!');
+    
+    // Retrieve the URL of the uploaded file
+    getDownloadURL(snapshot.ref).then((url) => {
+      console.log('File available at', url);
+      // Here you can call a function to update the UI with the new image URL
+    });
+  }).catch((error) => {
+    console.error("Error uploading image: ", error);
+  });
 };
