f505d77b8d36d47334dd3045f8158c7048f6df70
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-22 07:33:48 -0400

    Update loadImage.js

--- app/static/loadImage.js
+++ app/static/loadImage.js
@@ -11,3 +11,25 @@ window.onload = function() {
     imageElement.style.display = 'none'; // This hides the image element.
   }
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  const uploadButton = document.getElementById('uploadButton');
+  const imageUploadInput = document.getElementById('imageUpload');
+  
+  uploadButton.addEventListener('click', function() {
+    const file = imageUploadInput.files[0];
+    if (file) {
+      // Assuming you have a function to get the current user's ID
+      const userId = getCurrentUserId(); // Implement this function to get the logged-in user's ID
+      uploadImageToFirebase(userId, file);
+    } else {
+      console.log('No file selected!');
+    }
+  });
+});
+
+function getCurrentUserId() {
+  // This is a placeholder function. You need to replace it with actual logic to get the user's ID.
+  // For example, you might get it from the server session or a cookie.
+  return 'some-user-id'; // Replace with actual user ID retrieval logic
+}
